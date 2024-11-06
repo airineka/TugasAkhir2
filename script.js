991e4d6cf7bfa56a63587177847c419c784f896d
Airin <airin.eka.251@gmail.com>
2024-11-06 17:51:37 +0700

    6

--- script.js
+++
@@ -1,8 +0,0 @@
-let cart = [];
-let totalPrice = 0;
-
-function addToCart(productName, productPrice) {
-    cart.push({ name: productName, price: productPrice });
-    totalPrice += productPrice;
-    updateCart();
-}
