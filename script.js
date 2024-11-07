7cce4c78d675ecf3c00649d6cad1e980666fb333
Airin <airin.eka.251@gmail.com>
2024-11-07 17:54:53 +0700

    7

---
+++ script.js
@@ -0,0 +1,7 @@
+let cart = [];
+
+// menambahkan produk ke keranjang
+function addToCart(productName, productPrice, button) {
+    const quantityInput = button.previousElementSibling;
+    const quantity = parseInt(quantityInput.value);  
+}
