092cfda467d7b2943ea3448607607d10cef3144a
Airin <airin.eka.251@gmail.com>
2024-11-08 17:57:51 +0700

    8

--- script.js
+++ script.js
@@ -4,4 +4,15 @@ let cart = [];
 function addToCart(productName, productPrice, button) {
     const quantityInput = button.previousElementSibling;
     const quantity = parseInt(quantityInput.value);  
+
+    if (quantity <= 0) {
+        alert("Jumlah Produk Tidak Boleh Nol.");
+        return;
+    }
+
+    const existingProductIndex = cart.findIndex(item => item.name === productName);
+
+    if (existingProductIndex > -1) {
+      
+    }
 }
